0c125a37bd83baab6472207377fe5a8fb5a41390
Tharun Kumar <tharun@pop-os.localdomain>
2021-02-14 23:20:19 +0530

    improved code reusability

--- src/handler.rs
+++ src/handler.rs
@@ -1,65 +1,128 @@
-use std::time::Duration;
-
 use alertor::Alerter;
+use chrono::{DateTime, Local};
 use serde_json::Value;
+use std::time::Duration;
 
 use crate::{alertor, error::Errors};
 
 pub struct Handler<'a> {
     pub coins: Vec<Coin<'a>>,
     alertor: alertor::Alerter<'a>,
+    start_time: DateTime<Local>,
 }
 
 impl<'a> Handler<'a> {
     pub fn new(coins: Vec<Coin<'a>>, alertor: Alerter<'a>) -> Handler<'a> {
-        Handler { coins, alertor }
+        Handler {
+            coins,
+            alertor,
+            start_time: Local::now(),
+        }
     }
 
     pub async fn run(&mut self, data: Value) -> Result<(), Errors<'a>> {
-        for coin in self.coins.iter() {
-            let price = match data[coin.reference]["last"].as_str() {
-                Some(val) => val.parse::<f64>().unwrap_or(0.0),
-                None => 0.0,
-            };
-            let now = chrono::Local::now();
-            println!(
-                " [{:20}] Price: {:^10}{:>5}[{}]",
-                coin.name,
-                price,
-                "",
-                now.format("%b %-d %-I:%M").to_string()
-            );
-            if coin.validate(price) {
-                self.alertor.alert_mail(coin.name).await?;
-                self.alertor.alert_voice(coin.name).await;
+        for coin in self.coins.iter_mut() {
+            for exchange_details in coin.exchange_details.iter_mut() {
+                let price = match data[exchange_details.reference]["last"].as_str() {
+                    Some(val) => val.parse::<f64>().unwrap_or(0.0),
+                    None => 0.0,
+                };
+                exchange_details.update_price(price);
+                if exchange_details.validate() {
+                    self.alertor.alert_mail(coin.name).await?;
+                    self.alertor.alert_voice(coin.name).await;
+                };
             }
         }
-        println!("{:-<60}", "");
+        self.print_price().await;
+        let diff = Local::now().signed_duration_since(self.start_time);
+        println!(
+            "{:<48}Uptime [{:02}h::{:02}m::{:02}s]",
+            "",
+            if diff.num_hours() >= 24 {
+                diff.num_hours() % 24
+            } else {
+                diff.num_hours()
+            },
+            if diff.num_minutes() >= 60 {
+                diff.num_minutes() % 60
+            } else {
+                diff.num_minutes()
+            },
+            if diff.num_seconds() >= 60 {
+                diff.num_seconds() % 60
+            } else {
+                diff.num_seconds()
+            },
+        );
+        println!("{:-<70}", "");
         std::thread::sleep(Duration::from_secs(5));
         return Ok(());
+    }
+
+    pub async fn print_price(&self) {
+        for coin in self.coins.iter() {
+            let now = chrono::Local::now();
+            let mut stdout = vec![format!("[{}]{:^25}", now.format("%H:%M:%S"), coin.name)];
+            for exchange in coin.exchange_details.iter() {
+                if exchange.exchange_currency == "INR" {
+                    stdout.push(format!("₹ {:<20}", exchange.current_price.unwrap_or(0.0)));
+                } else {
+                    stdout.push(format!("$ {:<20}", exchange.current_price.unwrap_or(0.0)));
+                }
+            }
+            println!("{}", stdout.concat());
+        }
     }
 }
 
 #[allow(dead_code)]
 pub struct Coin<'a> {
-    reference: &'a str,
+    exchange_details: Vec<ExchangeDetails<'a>>,
     name: &'a str,
-    max_value: f64,
-    min_value: f64,
 }
 
 impl<'a> Coin<'a> {
-    pub fn new(reference: &'a str, name: &'a str, max_value: f64, min_value: f64) -> Coin<'a> {
+    pub fn new(name: &'a str, exchange_details: Vec<ExchangeDetails<'a>>) -> Coin<'a> {
         Coin {
-            reference,
             name,
-            max_value,
+            exchange_details,
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct ExchangeDetails<'a> {
+    reference: &'a str,
+    exchange_currency: &'a str,
+    min_value: f64,
+    max_value: f64,
+    current_price: Option<f64>,
+}
+
+impl<'a> ExchangeDetails<'a> {
+    pub fn new(
+        reference: &'a str,
+        exchange_currency: &'a str,
+        min_value: f64,
+        max_value: f64,
+    ) -> ExchangeDetails<'a> {
+        ExchangeDetails {
+            reference,
+            exchange_currency,
             min_value,
+            max_value,
+            current_price: None,
         }
     }
 
-    fn validate(&self, price: f64) -> bool {
-        if price >= self.max_value {
+    pub fn update_price(&mut self, price: f64) {
+        self.current_price = Some(price);
+    }
+
+    fn validate(&self) -> bool {
+        if self.current_price.unwrap() >= self.max_value {
             true
         } else {
             false
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use handler::Handler;
+use handler::{ExchangeDetails, Handler};
 use serde_json::Value;
 
 mod alertor;
@@ -9,24 +9,58 @@ use crate::handler::Coin;
 
 #[tokio::main]
 async fn main() {
-    let doge = Coin::new("dogeinr", "Doge Coin", 6.0, 1.0);
-    let uni = Coin::new("uniinr", "UniSwap Coin", 1700.0, 1.0);
-    let ada = Coin::new("adainr", "Cardadano(ADA) Coin", 72.0, 1.0);
+    let doge = Coin::new(
+        "Doge Coin",
+        vec![
+            ExchangeDetails::new("dogeinr", "INR", 1.0, 5.2),
+            ExchangeDetails::new("dogeusdt", "USDT", 1.0, 0.07),
+        ],
+    );
+    let uni = Coin::new(
+        "UniSwap Coin",
+        vec![
+            ExchangeDetails::new("uniinr", "INR", 1.0, 1660.0),
+            ExchangeDetails::new("uniusdt", "USDT", 1.0, 22.5),
+        ],
+    );
+    let ada = Coin::new(
+        "Cardadano(ADA) Coin",
+        vec![
+            ExchangeDetails::new("adainr", "INR", 1.0, 68.0),
+            ExchangeDetails::new("adausdt", "USDT", 0.1, 0.96),
+        ],
+    );
+
+    let wst = Coin::new(
+        "Waltonchain Coin",
+        vec![ExchangeDetails::new("wtcusdt", "USDT", 0.1, 1.65)],
+    );
+
     let alert = match alertor::Alerter::new("tharunkumar77tk@gmail.com") {
         Ok(val) => val,
         Err(e) => return println!("{}", e),
     };
-    let mut handler = Handler::new(vec![doge, uni, ada], alert);
+    let mut handler = Handler::new(vec![doge, uni, ada, wst], alert);
     loop {
-        let resp = reqwest::Client::new()
+        let resp = match reqwest::Client::new()
             .get("https://api.wazirx.com/api/v2/tickers")
             .send()
             .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
-        let data: Value = serde_json::from_str(&resp).unwrap();
+        {
+            Ok(val) => val,
+            Err(e) => {
+                println!("[-] CANNOT PROCESS RESPONSE {}", e.to_string());
+                continue;
+            }
+        };
+        let text = match resp.text().await {
+            Ok(val) => val,
+            Err(e) => {
+                println!("[-] CANNOT PROCESS RESPONSE {}", e.to_string());
+                continue;
+            }
+        };
+        let data: Value = serde_json::from_str(&text).unwrap();
 
         match handler.run(data).await {
             Ok(_) => continue,
